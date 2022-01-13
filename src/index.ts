cbec3b310ee78b525b842bb60d5355556877c842
unsu0707 <unsu0707@gmail.com>
2022-01-13 18:27:13 +0900

    use database

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 const { App } = require("@slack/bolt");
+import db from "./db";
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-app.message("goodbye", async ({ message, say }) => {
+app.message("deus post ([^ ]+)", async ({ message, say, context }) => {
+  const { posts } = db.data;
+  const key: string = context.matches[1];
+  const post = posts.find((p) => p.id === req.params.id);
+  var result: boolean = await lend();
+  await say(`See ya later, <@${message.user}> :wave:`);
+});
+
+app.message("deus get ([^ ]+)", async ({ message, say }) => {
+  var result: boolean = await lend();
   await say(`See ya later, <@${message.user}> :wave:`);
 });
 
@@ -14,3 +24,5 @@ app.message("goodbye", async ({ message, say }) => {
 
   console.log("⚡️ Bolt app is running!");
 })();
+
+function lend(): boolean | PromiseLike<boolean> {}
